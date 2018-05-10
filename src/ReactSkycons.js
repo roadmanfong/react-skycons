eabc4339c4a70baebd9e8e9d9d18600f33207e25
ac <ac@domain.local>
2018-05-10 14:44:39 +0900

    add lowercase icon names

--- src/ReactSkycons.js
+++ src/ReactSkycons.js
@@ -1,23 +1,33 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import ReactDOM from 'react-dom'
-const Skycons = require('skycons')(window)
+import React from "react";
+import PropTypes from "prop-types";
+import ReactDOM from "react-dom";
+const Skycons = require("skycons")(window);
 
 export default class ReactSkycons extends React.Component {
   static propTypes = {
     color: PropTypes.string,
     autoplay: PropTypes.bool,
     icon: PropTypes.oneOf([
-      'CLEAR_DAY',
-      'CLEAR_NIGHT',
-      'PARTLY_CLOUDY_DAY',
-      'PARTLY_CLOUDY_NIGHT',
-      'CLOUDY',
-      'RAIN',
-      'SLEET',
-      'SNOW',
-      'WIND',
-      'FOG'
+      "CLEAR_DAY",
+      "CLEAR_NIGHT",
+      "PARTLY_CLOUDY_DAY",
+      "PARTLY_CLOUDY_NIGHT",
+      "CLOUDY",
+      "RAIN",
+      "SLEET",
+      "SNOW",
+      "WIND",
+      "FOG",
+      "clear-day",
+      "clear-night",
+      "partly-cloudy-day",
+      "partly-cloudy-night",
+      "cloudy",
+      "rain",
+      "sleet",
+      "snow",
+      "wind",
+      "fog"
     ])
   };
 
@@ -26,42 +36,42 @@ export default class ReactSkycons extends React.Component {
     autoplay: true
   };
 
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
-      skycons: new Skycons({ 'color': this.props.color })
-    }
+      skycons: new Skycons({ color: this.props.color })
+    };
   }
 
-  componentDidMount () {
-    const { skycons } = this.state
-    skycons.add(ReactDOM.findDOMNode(this), Skycons[this.props.icon])
+  componentDidMount() {
+    const { skycons } = this.state;
+    skycons.add(ReactDOM.findDOMNode(this), Skycons[this.props.icon]);
 
     if (this.props.autoplay) {
-      skycons.play()
+      skycons.play();
     }
   }
 
-  componentWillReceiveProps (nextProps) {
-    this.state.skycons.set(ReactDOM.findDOMNode(this), Skycons[nextProps.icon])
+  componentWillReceiveProps(nextProps) {
+    this.state.skycons.set(ReactDOM.findDOMNode(this), Skycons[nextProps.icon]);
   }
 
-  componentWillUnmount () {
-    const { skycons } = this.state
-    skycons.pause()
-    skycons.remove(ReactDOM.findDOMNode(this))
+  componentWillUnmount() {
+    const { skycons } = this.state;
+    skycons.pause();
+    skycons.remove(ReactDOM.findDOMNode(this));
   }
 
-  play () {
-    this.state.skycons.play()
+  play() {
+    this.state.skycons.play();
   }
 
-  pause () {
-    this.state.skycons.pause()
+  pause() {
+    this.state.skycons.pause();
   }
 
-  render () {
+  render() {
     const {
       /* eslint-disable */
       // to remove unnecessary props
@@ -70,15 +80,13 @@ export default class ReactSkycons extends React.Component {
       icon,
       /* eslint-enable */
       ...restPops
-    } = this.props
+    } = this.props;
 
     const defaultStyle = {
-      width: '100%',
-      height: '100%'
-    }
+      width: "100%",
+      height: "100%"
+    };
 
-    return (
-      <canvas style={defaultStyle} {...restPops} />
-    )
+    return <canvas style={defaultStyle} {...restPops} />;
   }
 }

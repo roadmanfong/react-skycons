dc80ba5cd50100d764f1508e694a0449c512509d
roadman <waneblade@gmail.com>
2020-09-20 01:01:48 +0800

    update testing library

--- tests/Skycons.spec.tsx
+++ tests/Skycons.spec.tsx
@@ -4,6 +4,30 @@ import '@testing-library/jest-dom/extend-expect'
 import Skycons, { SkyconsType } from '../src/Skycons'
 
 it('should render a component', async () => {
-  render(<Skycons type={SkyconsType.CLEAR_DAY} />)
+  render(
+    <Skycons type={SkyconsType.CLEAR_DAY} color="red" resizeClear={true} />
+  )
+  expect(screen.queryByRole('img')).toBeInTheDocument()
+})
+
+it('should be able to re-render a component', async () => {
+  const { rerender } = render(
+    <Skycons
+      type={SkyconsType.CLEAR_DAY}
+      color="red"
+      animate={true}
+      size={24}
+      resizeClear={true}
+    />
+  )
+  rerender(
+    <Skycons
+      type={SkyconsType.CLEAR_NIGHT}
+      color="blue"
+      animate={false}
+      size={48}
+      resizeClear={false}
+    />
+  )
   expect(screen.queryByRole('img')).toBeInTheDocument()
 })
